41eb339504761e9c72355408fd446f0f468e7e05
Lukas <lukastolz9@gmail.com>
2021-12-08 19:58:11 +0100

    Added Challenge 3.2

--- AdventOfCode2021/BinaryDiagnostic/BinaryDiagnostic.cs
+++ AdventOfCode2021/BinaryDiagnostic/BinaryDiagnostic.cs
@@ -39,7 +39,31 @@ namespace AdventOfCode2021
 
         public Int32 SolveSecond()
         {
-            return 0;
+            List<String> oxygenList = System.IO.File.ReadAllLines(path).ToList();
+            List<String> scrubberList = new(oxygenList);
+
+            Int32 bitLength = oxygenList.FirstOrDefault().Length;
+            Int32 oxygen = 0;
+            Int32 scrubber = 0;            
+            for(int i = 0; i < bitLength; i++)
+            {
+                Int32 oxcounter = 0;
+                Int32 sccounter = 0;
+                foreach(var value in oxygenList)
+                    oxcounter += value[i] == '1' ? 1 : -1;
+                foreach(var value in scrubberList)
+                    sccounter += value[i] == '1' ? 1 : -1;
+                
+                oxygenList.RemoveAll(x => x[i] == (oxcounter >= 0 ? '0' : '1'));
+                scrubberList.RemoveAll(x => x[i] == (sccounter >= 0 ? '1' : '0'));
+
+                if(oxygenList.Count() == 1)
+                    oxygen = Convert.ToInt32(oxygenList.FirstOrDefault(), 2);
+                if(scrubberList.Count() == 1)
+                    scrubber = Convert.ToInt32(scrubberList.FirstOrDefault(), 2);
+            }
+
+            return oxygen * scrubber;
         }
     }
 }
